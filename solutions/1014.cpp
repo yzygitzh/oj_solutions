53af38f84a3932a60f93518cb5dfb56bdcf7f751
Ziyue Yang <yzylivezh@hotmail.com>
2017-01-29 20:44:11 +0800

    finish 1014

--- solutions/1014.cpp
+++ solutions/1014.cpp
@@ -4,57 +4,67 @@
  * A backpack problem with
  * total volume N = sum(ni) / 2
  *
+ * Using binary optimization to prevent TLE
+ * The same marbles can be represented using "the power of 2"'s
+ * O(N * log(K))
+ *
+ * Using 1-dim array to satisfy memory limit
  */
 
 #include <cstdio>
 
-int F[6][20001], flatN[20001];
+int F[120001], flatN[100];
 
 void calcF(int maxIdx, int maxVol, int sampleCount) {
     int nIdx = 1, currVol = 0;
-    for (int vol = 0; vol <= maxVol; vol++)
-        for (int k = 0; k < maxIdx; k++) {
-            int val1 = (k - 1 >= 0) ? F[vol % 6][k - 1] : 0;
+    for (int k = 1; k < maxIdx; k++)
+        for (int vol = maxVol; vol >= 0; vol--) {
+            int val1 = F[vol];
             int val2 = 0;
-            if (vol - flatN[k] >= 0) {
-                val2 += flatN[k];
-                if (k - 1 >= 0)
-                    val2 += F[(vol - flatN[k] + 6) % 6][k - 1];
+            if (vol - flatN[k] >= 0)
+                val2 = flatN[k] + F[vol - flatN[k]];
+            F[vol] = (val1 > val2) ? val1 : val2;
+            if (F[vol] == maxVol) {
+                printf("Collection #%d:\nCan be divided.\n\n", sampleCount);
+                return;
             }
-            F[vol % 6][k] = (val1 > val2) ? val1 : val2;
-        }
-
-    for (int k = 0; k < maxIdx; k++)
-        if (F[maxVol % 6][k] == maxVol) {
-            printf("Collection #%d:\nCan be divided.\n\n", sampleCount);
-            return;
         }
     printf("Collection #%d:\nCan't be divided.\n\n", sampleCount);
     return;
 }
 
 int main() {
-    int n[7], maxIdx, maxVol, sampleCount = 0;
+    int sampleCount = 0;
     while (1) {
-        sampleCount++;
-
-        scanf("%d %d %d %d %d %d", &n[1], &n[2], &n[3], &n[4], &n[5], &n[6]);
-        maxVol = n[1] + 2 * n[2] + 3 * n[3] + 4 * n[4] + 5 * n[5] + 6 * n[6];
+        int n[7], maxVol = 0;
+        for (int i = 1; i <= 6; i++) {
+            scanf("%d", &n[i]);
+            maxVol += i * n[i];
+        }
         if (maxVol == 0)
             break;
-        maxIdx = 0;
-        for (int i = 1; i < 7;)
-            if (n[i] == 0)
-                i++;
-            else {
-                flatN[maxIdx++] = i;
-                n[i]--;
-            }
 
-        if (maxVol % 6)
-            printf("Collection #%d:\nCan't be divided.\n\n", sampleCount);
+        for (int i = 0; i < 120001; i++)
+            F[i] = 0;
+        for (int i = 0; i < 100; i++)
+            flatN[i] = 0;
+
+        int maxIdx = 0;
+        for (int i = 1; i <= 6; i++) {
+            int binary = 1;
+            while (n[i] > 0) {
+                n[i] -= binary;
+                if (n[i] < 0)
+                    flatN[maxIdx++] = i * (n[i] + binary);
+                else
+                    flatN[maxIdx++] = i * binary;
+                binary *= 2;
+            }
+        }
+        if (maxVol % 2)
+            printf("Collection #%d:\nCan't be divided.\n\n", ++sampleCount);
         else
-            calcF(maxIdx, maxVol / 2, sampleCount);
+            calcF(maxIdx, maxVol / 2, ++sampleCount);
     }
     return 0;
 }
